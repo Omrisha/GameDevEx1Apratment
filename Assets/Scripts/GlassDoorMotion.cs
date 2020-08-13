e1d6e55ab0fc3abf00e3869e26c10906a82ccdeb
Omri Shapira <omri.shapira@gmail.com>
2020-08-13 21:42:32 +0300

    Adding doors + animations and door script motion + Terrain

---
+++ Assets/Scripts/GlassDoorMotion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassDoorMotion : MonoBehaviour
+{
+    public Animator animator;
+    public bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("GlassOpen");
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isOpen)
+        {
+            animator.SetTrigger("GlassClose");
+            isOpen = false;
+        }
+    }
+}
